3abc7e6a27c7daffc0906932ef99df05d4389c09
Ian <ian@foo.bar>
2013-04-03 15:21:04 +0100

    A little refactoring to tidy up the names of the arrays to make the code clearer.

--- src/roman/numerals/Roman.java
+++ src/roman/numerals/Roman.java
@@ -2,15 +2,15 @@ package roman.numerals;
 
 public class Roman
 {
-	private String[] numerals = { "X", "IX", "V", "IV", "I" };
-	private int[]      values = {  10,   9,   5,    4,   1  };
+	private String[] numerals   = { "X", "IX", "V", "IV", "I" };
+	private int[] numeralValues = {  10,   9,   5,    4,   1  };
 
 	public String generate(int number)
 	{
 		for (int i = 0; i < numerals.length; i++)
 		{
-			if (number >= values[i])
-				return numerals[i] + generate(number - values[i]);
+			if (number >= numeralValues[i])
+				return numerals[i] + generate(number - numeralValues[i]);
 		}
 
 		return "";
