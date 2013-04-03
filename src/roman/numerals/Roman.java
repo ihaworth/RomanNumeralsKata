8dbcbdd82df8e3b613ed928ff2f0659de22ad415
Ian <ian@foo.bar>
2013-04-03 15:06:27 +0100

    Refactor to reduce the duplication between the cases of handling each numeral by looping over a list of numerals and their values.  Note that this is a part-way stage in the refactoring process.  There are still code smells with this code, for example I've had to return a null value at the end of the method, which will never get called (as long as we have valid input), and there is possibly an issue with having the Roman Numerals and their values in 2 arrays so they aren't closely associated (in technical terms, the arrays are coupled and modifying one means you should modify the other).  I hope to address these issues in subsequent commits.

--- src/roman/numerals/Roman.java
+++ src/roman/numerals/Roman.java
@@ -2,38 +2,20 @@ package roman.numerals;
 
 public class Roman
 {
+	private String[] numerals = { "X", "IX", "V", "IV", "I" };
+	private int[]      values = {  10,   9,   5,    4,   1  };
+
 	public String generate(int number)
 	{
-		if (number > 10)
-			return generate(10) + generate(number - 10);
-		
-		if (number == 10)
-			return "X";
-		
-		
-		if (number > 9)
-			return generate(9) + generate(number - 9);
-		
-		if (number == 9)
-			return "IX";
-		
-		
-		if (number > 5)
-			return generate(5) + generate(number - 5);
-		
-		if (number == 5)
-			return "V";
-		
-		if (number > 4)
-			return generate(4) + generate(number - 4);
-		
-		if (number == 4)
-			return "IV";
-		
-		
-		if (number > 1)
-			return generate(1) + generate(number - 1);
-		
-		return "I";
+		for (int i = 0; i < numerals.length; i++)
+		{
+			if (number > values[i])
+				return generate(values[i]) + generate(number - values[i]);
+			
+			if (number == values[i])
+				return numerals[i];
+		}
+
+		return null;
 	}
 }
