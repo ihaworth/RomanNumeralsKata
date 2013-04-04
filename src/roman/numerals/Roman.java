7192af31ef8371cc9c5d26183123108fb7c04c87
Ian <ian@foo.bar>
2013-04-04 11:40:52 +0100

    Change tabs to spaces to make the code more readable on github.

--- src/roman/numerals/Roman.java
+++ src/roman/numerals/Roman.java
@@ -2,17 +2,17 @@ package roman.numerals;
 
 public class Roman
 {
-	private String[] numerals   = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-	private int[] numeralValues = { 1000, 900, 500,  400, 100,  90,   50,  40,   10,   9,   5,    4,   1  };
+    private String[] numerals   = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    private int[] numeralValues = { 1000, 900, 500,  400, 100,  90,   50,  40,   10,   9,   5,    4,   1  };
 
-	public String generate(int number)
-	{
-		for (int i = 0; i < numerals.length; i++)
-		{
-			if (number >= numeralValues[i])
-				return numerals[i] + generate(number - numeralValues[i]);
-		}
+    public String generate(int number)
+    {
+        for (int i = 0; i < numerals.length; i++)
+        {
+            if (number >= numeralValues[i])
+                return numerals[i] + generate(number - numeralValues[i]);
+        }
 
-		return "";
-	}
+        return "";
+    }
 }
--- tests/roman/numerals/RomanNumeralsTest.java
+++ tests/roman/numerals/RomanNumeralsTest.java
@@ -8,73 +8,73 @@ import org.junit.Test;
 
 public class RomanNumeralsTest
 {
-	@Test
-	public void checkIntegerToRomanNumeralConversion()
-	{
-		assertThatIntegerGivesRoman(1, "I");
-		assertThatIntegerGivesRoman(2, "II");
-		assertThatIntegerGivesRoman(3, "III");
-		assertThatIntegerGivesRoman(5, "V");
-		assertThatIntegerGivesRoman(4, "IV");
-		assertThatIntegerGivesRoman(6, "VI");
-		assertThatIntegerGivesRoman(7, "VII");
-		assertThatIntegerGivesRoman(8, "VIII");
-		assertThatIntegerGivesRoman(10, "X");
-		assertThatIntegerGivesRoman(9, "IX");
-		assertThatIntegerGivesRoman(11, "XI");
-		assertThatIntegerGivesRoman(12, "XII");
-		assertThatIntegerGivesRoman(13, "XIII");
-		assertThatIntegerGivesRoman(14, "XIV");
-		assertThatIntegerGivesRoman(15, "XV");
-		assertThatIntegerGivesRoman(16, "XVI");
-		assertThatIntegerGivesRoman(17, "XVII");
-		assertThatIntegerGivesRoman(18, "XVIII");
-		assertThatIntegerGivesRoman(19, "XIX");
-		assertThatIntegerGivesRoman(20, "XX");
-		assertThatIntegerGivesRoman(25, "XXV");
-		assertThatIntegerGivesRoman(29, "XXIX");
-		assertThatIntegerGivesRoman(30, "XXX");
-		assertThatIntegerGivesRoman(34, "XXXIV");
-		assertThatIntegerGivesRoman(36, "XXXVI");
-		assertThatIntegerGivesRoman(39, "XXXIX");
-		assertThatIntegerGivesRoman(40, "XL");
-		assertThatIntegerGivesRoman(50, "L");
-		assertThatIntegerGivesRoman(41, "XLI");
-		assertThatIntegerGivesRoman(44, "XLIV");
-		assertThatIntegerGivesRoman(48, "XLVIII");
-		assertThatIntegerGivesRoman(49, "XLIX");
-		assertThatIntegerGivesRoman(53, "LIII");
-		assertThatIntegerGivesRoman(57, "LVII");
-		assertThatIntegerGivesRoman(60, "LX");
-		assertThatIntegerGivesRoman(64, "LXIV");
-		assertThatIntegerGivesRoman(69, "LXIX");
-		assertThatIntegerGivesRoman(70, "LXX");
-		assertThatIntegerGivesRoman(72, "LXXII");
-		assertThatIntegerGivesRoman(75, "LXXV");
-		assertThatIntegerGivesRoman(77, "LXXVII");
-		assertThatIntegerGivesRoman(80, "LXXX");
-		assertThatIntegerGivesRoman(83, "LXXXIII");
-		assertThatIntegerGivesRoman(85, "LXXXV");
-		assertThatIntegerGivesRoman(88, "LXXXVIII");
-		assertThatIntegerGivesRoman(89, "LXXXIX");
-		assertThatIntegerGivesRoman(90, "XC");
-		assertThatIntegerGivesRoman(100, "C");
-		assertThatIntegerGivesRoman(400, "CD");
-		assertThatIntegerGivesRoman(500, "D");
-		assertThatIntegerGivesRoman(900, "CM");
-		assertThatIntegerGivesRoman(1000, "M");
-		assertThatIntegerGivesRoman(99, "XCIX");
-		assertThatIntegerGivesRoman(399, "CCCXCIX");
-		assertThatIntegerGivesRoman(499, "CDXCIX");
-		assertThatIntegerGivesRoman(899, "DCCCXCIX");
-		assertThatIntegerGivesRoman(999, "CMXCIX");
-		assertThatIntegerGivesRoman(1998, "MCMXCVIII");
-		assertThatIntegerGivesRoman(2000, "MM");
-		assertThatIntegerGivesRoman(3999, "MMMCMXCIX");
-	}
+    @Test
+    public void checkIntegerToRomanNumeralConversion()
+    {
+        assertThatIntegerGivesRoman(1, "I");
+        assertThatIntegerGivesRoman(2, "II");
+        assertThatIntegerGivesRoman(3, "III");
+        assertThatIntegerGivesRoman(5, "V");
+        assertThatIntegerGivesRoman(4, "IV");
+        assertThatIntegerGivesRoman(6, "VI");
+        assertThatIntegerGivesRoman(7, "VII");
+        assertThatIntegerGivesRoman(8, "VIII");
+        assertThatIntegerGivesRoman(10, "X");
+        assertThatIntegerGivesRoman(9, "IX");
+        assertThatIntegerGivesRoman(11, "XI");
+        assertThatIntegerGivesRoman(12, "XII");
+        assertThatIntegerGivesRoman(13, "XIII");
+        assertThatIntegerGivesRoman(14, "XIV");
+        assertThatIntegerGivesRoman(15, "XV");
+        assertThatIntegerGivesRoman(16, "XVI");
+        assertThatIntegerGivesRoman(17, "XVII");
+        assertThatIntegerGivesRoman(18, "XVIII");
+        assertThatIntegerGivesRoman(19, "XIX");
+        assertThatIntegerGivesRoman(20, "XX");
+        assertThatIntegerGivesRoman(25, "XXV");
+        assertThatIntegerGivesRoman(29, "XXIX");
+        assertThatIntegerGivesRoman(30, "XXX");
+        assertThatIntegerGivesRoman(34, "XXXIV");
+        assertThatIntegerGivesRoman(36, "XXXVI");
+        assertThatIntegerGivesRoman(39, "XXXIX");
+        assertThatIntegerGivesRoman(40, "XL");
+        assertThatIntegerGivesRoman(50, "L");
+        assertThatIntegerGivesRoman(41, "XLI");
+        assertThatIntegerGivesRoman(44, "XLIV");
+        assertThatIntegerGivesRoman(48, "XLVIII");
+        assertThatIntegerGivesRoman(49, "XLIX");
+        assertThatIntegerGivesRoman(53, "LIII");
+        assertThatIntegerGivesRoman(57, "LVII");
+        assertThatIntegerGivesRoman(60, "LX");
+        assertThatIntegerGivesRoman(64, "LXIV");
+        assertThatIntegerGivesRoman(69, "LXIX");
+        assertThatIntegerGivesRoman(70, "LXX");
+        assertThatIntegerGivesRoman(72, "LXXII");
+        assertThatIntegerGivesRoman(75, "LXXV");
+        assertThatIntegerGivesRoman(77, "LXXVII");
+        assertThatIntegerGivesRoman(80, "LXXX");
+        assertThatIntegerGivesRoman(83, "LXXXIII");
+        assertThatIntegerGivesRoman(85, "LXXXV");
+        assertThatIntegerGivesRoman(88, "LXXXVIII");
+        assertThatIntegerGivesRoman(89, "LXXXIX");
+        assertThatIntegerGivesRoman(90, "XC");
+        assertThatIntegerGivesRoman(100, "C");
+        assertThatIntegerGivesRoman(400, "CD");
+        assertThatIntegerGivesRoman(500, "D");
+        assertThatIntegerGivesRoman(900, "CM");
+        assertThatIntegerGivesRoman(1000, "M");
+        assertThatIntegerGivesRoman(99, "XCIX");
+        assertThatIntegerGivesRoman(399, "CCCXCIX");
+        assertThatIntegerGivesRoman(499, "CDXCIX");
+        assertThatIntegerGivesRoman(899, "DCCCXCIX");
+        assertThatIntegerGivesRoman(999, "CMXCIX");
+        assertThatIntegerGivesRoman(1998, "MCMXCVIII");
+        assertThatIntegerGivesRoman(2000, "MM");
+        assertThatIntegerGivesRoman(3999, "MMMCMXCIX");
+    }
 
-	private void assertThatIntegerGivesRoman(int number, String expectedRoman)
-	{
-		assertThat("Converting " + number, new Roman().generate(number), is(equalTo(expectedRoman)));
-	}
+    private void assertThatIntegerGivesRoman(int number, String expectedRoman)
+    {
+        assertThat("Converting " + number, new Roman().generate(number), is(equalTo(expectedRoman)));
+    }
 }
